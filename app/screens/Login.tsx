b769883383235b27fae0994801598f5504439a7d
Calvin Ng <xdurpfishh@gmail.com>
2024-05-31 18:36:51 +0800

    login page

--- app/screens/Login.tsx
+++ app/screens/Login.tsx
@@ -1,5 +1,5 @@
-import { View, Text, StyleSheet, TextInput, Button, ActivityIndicator, KeyboardAvoidingView } from 'react-native'
-import React, {useState} from 'react'
+import { View, Text, StyleSheet, TextInput, ActivityIndicator, KeyboardAvoidingView, Image, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
 import { FIREBASE_AUTH } from '../../firebaseConfig'
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
 
@@ -14,13 +14,13 @@ const Login = () => {
         try {
             const response = await signInWithEmailAndPassword(auth, email, password);
             console.log(response);
-        }   catch (error: any) {
+        } catch (error: any) {
             console.log(error);
             alert('Sign in failed!' + error.message);
-        }   finally {
+        } finally {
             setLoading(false);
         }
-        
+
     }
 
     const signUp = async () => {
@@ -29,37 +29,45 @@ const Login = () => {
             const response = await createUserWithEmailAndPassword(auth, email, password);
             console.log(response);
             alert('Check your emails!');
-        }   catch (error: any) {
+        } catch (error: any) {
             console.log(error);
             alert('Sign up failed!' + error.message);
-        }   finally {
+        } finally {
             setLoading(false);
         }
-        
+
     }
 
     return (
-        <View style ={styles.container}>
+        <View style={styles.container}>
             <KeyboardAvoidingView behavior="padding">
-            <TextInput value={email} style={styles.input} placeholder="Email" autoCapitalize="none" onChangeText = {(text => setEmail(text))} /> 
-            <TextInput secureTextEntry={true} value={password} style={styles.input} placeholder="Password" autoCapitalize="none" onChangeText = {(text => setPassword(text))}/>  
-
-            {loading ? (<ActivityIndicator size="large" color="#0000ff"/> ) : (
-                <>
-                <Button title="Login" onPress={signIn} />
-                <Button title="Create account" onPress={signUp}/>
-                </>
-            )}
-        </KeyboardAvoidingView>
+                <View style={styles.logoContainer}>
+                    <Image source={require('../../assets/crossroadsLogo.png')} style={styles.logo} />
+                </View>
+                <TextInput value={email} style={styles.input} placeholder="Email" autoCapitalize="none" onChangeText={(text) => setEmail(text)} />
+                <TextInput secureTextEntry={true} value={password} style={styles.input} placeholder="Password" autoCapitalize="none" onChangeText={(text) => setPassword(text)} />
+    
+                {loading ? (<ActivityIndicator size="large" color="#0000ff" />) : (
+                    <View style={styles.buttonContainer}>
+                        <TouchableOpacity style={styles.button} onPress={signIn}>
+                            <Text style={styles.buttonText}>Login</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.button} onPress={signUp}>
+                            <Text style={styles.buttonText}>Create account</Text>
+                        </TouchableOpacity>
+                    </View>
+                )}
+            </KeyboardAvoidingView>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        marginHorizontal:20,
-        flex:1,
-        justifyContent: 'center'
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
     },
     input: {
         marginVertical: 4,
@@ -68,7 +76,33 @@ const styles = StyleSheet.create({
         borderRadius: 4,
         padding: 10,
         backgroundColor: '#fff'
-    }
-})
+    },
+    logoContainer: {
+        alignItems: 'center',
+        marginBottom: 20,
+    },
+    logo: {
+        width: 200,
+        height: 200,
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 20,
+    },
+    button: {
+        backgroundColor: '#72bcd4',
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        marginHorizontal: 5,
+        borderRadius: 5,
+    },
+    buttonText: {
+        color: '#FFFFFF',
+        textAlign: 'center',
+        fontWeight: 'bold',
+    },
+});
 
 export default Login
